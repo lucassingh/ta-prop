71a04fa55bf057ef677ed8a86a09d6667b6a67ae
lucas.singh <lucas.singh@bizitglobal.com>
2023-06-29 15:10:38 -0300

    first commitsss

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import './footer.scss'
 
 const Footer = () => {
     return (
-        <div className='footer-section'><span>&#169; 2023 THE ARBORIST. All rights reserved</span></div>
+        <div className='footer-section'><span>&#169; 2023 THE ARBORIST. All rights reserved </span></div>
     )
 }
 
